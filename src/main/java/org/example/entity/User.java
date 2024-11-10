26ae525042e94b3170c22509754c4636feef3d4a
shararaeh <your.email@example.com>
2024-11-10 17:06:27 +0100

    Entity For Users with properties like (username,email,password,bio,display name, current date for create user)

---
+++ src/main/java/org/example/entity/User.java
@@ -0,0 +1,36 @@
+package org.example.entity;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+
+public class User {
+    private Long id;
+    private String userName;
+    private String password;
+    private String email;
+    private String bio;
+    private String displayName;
+    private LocalDateTime createAt;
+
+    public User(String userName, String password, String email, String bio, String displayName) {
+        this.userName = userName;
+        this.password = password;
+        this.email = email;
+        this.bio = bio;
+        this.displayName = displayName;
+
+    }
+
+    @Override
+    public String toString() {
+        return
+                        "UserName :" + userName + "\t" +
+                        "Password :" + password + "\t" +
+                        "Email :" + email + "\t" +
+                        "Bio :" + bio + "\t" +
+                        "DisplayName :" + displayName;
+    }
+}
